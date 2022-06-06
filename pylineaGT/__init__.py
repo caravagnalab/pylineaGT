9f74701e44745a8cf977e41970db3e1cf516b38e
elena-buscaroli <ele.buscaroli@gmail.com>
2022-06-06 17:55:01 +0200

    mc

--- pylineaGT/__init__.py
+++ pylineaGT/__init__.py
@@ -1,4 +1,4 @@
-# import .mvnmm
-# import .explogreg
-# import .run_inference
-# import .simulations
+from .mvnmm import MVNMixtureModel
+from .explogreg import Regression
+from .run_inference import run_inference
+from .simulations import Simulate, generate_synthetic_data
